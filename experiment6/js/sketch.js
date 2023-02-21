75805ccf88ae6f6a8b42af3089c87d9f1cdd3538
domberardi10 <domberardi10@gmail.com>
2023-02-20 22:40:22 -0800

    exp 6 files

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,67 +1,181 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// P_3_2_3_01
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+/**
+ * fontgenerator with dynamic elements. letter ouline consist of linked agents.
+ *
+ * MOUSE
+ * press + position x  : letter distortion
+ *
+ * KEYS
+ * a-z                 : text input (keyboard)
+ * alt                 : freeze current state
+ * del, backspace      : clear screen
+ * ctrl                : save png
+ */
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+const grammarJSON = {
+  "origin": ["#question# #identity# #action#?"],
+  "question": ["Who", "What", "Why", "How"],
+  "identity": ["are you", "am I", "are they", "is he", "is she"],
+  "action": ["typing", "thinking", "writing", "saying"]
+  };
 
-// Globals
-let myInstance;
-let canvasContainer;
+let grammar;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+var typedKey = 'a';
+var fontPath;
+
+var spacing = 20;
+var spaceWidth = 80; // width of letter ' '
+var fontSize = 100;
+var lineSpacing = fontSize * 1.2;
+var textW = 0;
+var letterX = 50 + spacing;
+var letterY = lineSpacing;
+
+var stepSize = 2;
+var danceFactor = 1;
+
+var font;
+var pnts;
+
+var freeze = false;
+
+var grammarTimer = 0;
+var currentSentence;
+
+class Sentence{
+  constructor(words, x, y, version){
+    this.words = words;
+    this.x = x;
+    this.y = y;
+    this.version = version;
+  }
+
+  drawSentence(){
+    for(let j = 0; j < this.words.length; j++){
+      let pnts = this.getPoints(this.words[j], j * 75);
+      if (pnts.length > 0) {
+        // let the points dance
+
+        // for (let i = 0; i < pnts.length; i++) {
+        //   pnts[i].x += noise(frameCount * 50 / mouseX) * 100;
+        //   pnts[i].y += noise(frameCount * 50 / mouseY) * 100;
+        // }
+        
+        pnts = this.movePoints(pnts);
+        
+        strokeWeight(0.08);
+    
+        beginShape();
+        // start controlpoint
+        curveVertex(pnts[pnts.length-1].x, pnts[pnts.length-1].y);
+        // only these points are drawn
+        for (var i = 0; i < pnts.length; i++) {
+          curveVertex(pnts[i].x, pnts[i].y);
+        }
+        curveVertex(pnts[0].x, pnts[0].y);
+        // end controlpoint
+        curveVertex(pnts[1].x, pnts[1].y);
+        endShape();
+        
+      }
     }
+  }
 
-    myMethod() {
-        // code to run when method is called
+  getPoints(letter, offset){
+    fontPath = font.getPath(letter, this.x + offset, this.y, 100);
+    var path = new g.Path(fontPath.commands);
+    path = g.resampleByLength(path, 25);
+    textW = path.bounds().width;
+    // remove all commands without a coordinate
+    for (var i = path.commands.length - 1; i >= 0 ; i--) {
+      if (path.commands[i].x == undefined) {
+        path.commands.splice(i, 1);
+      }
     }
+    return path.commands;
+  }
+
+  movePoints(pnts){
+    switch(this.version){
+      case 0:
+        for (let i = 0; i < pnts.length; i++) {
+          pnts[i].x += noise(frameCount * 50 / mouseX) * 100;
+          pnts[i].y += noise(frameCount * 50 / mouseY) * 100;
+        }
+        return pnts;
+      case 1:
+        for (let i = 0; i < pnts.length; i++) {
+          pnts[i].x += noise(frameCount * 50) * 100;
+          pnts[i].y += noise(frameCount * 50) * 100;
+        }
+        return pnts;
+      case 2:
+        for (let i = 0; i < pnts.length; i++) {
+          pnts[i].x += noise(frameCount / 50) * 100;
+          pnts[i].y += noise(frameCount / 50) * 100;
+        }
+        return pnts;
+      case 3:
+        for (let i = 0; i < pnts.length; i++) {
+          pnts[i].x += noise(frameCount * (i * 5)) * 100;
+          pnts[i].y += noise(frameCount * (i * 5)) * 100;
+        }
+        return pnts;
+      case 4:
+        for (let i = 0; i < pnts.length; i++) {
+          pnts[i].x += noise(frameCount * 20) * 50;
+        }
+        return pnts;
+    }
+  }
 }
 
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  createCanvas(windowWidth, windowHeight);
+  grammar = tracery.createGrammar(grammarJSON);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  opentype.load('data/FreeSansNoPunch.otf', function(err, f) {
+    if (err) {
+      print(err);
+    } else {
+      font = f;
+    }
+  });
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  if (!font) return;
+  
+  push();
+  noFill();
+  grammarTimer += deltaTime;
+  if (grammarTimer > 5000){
+    background(255);
+    let output = grammar.flatten("#origin#");
+    currentSentence = new Sentence(output, 300, windowHeight / 2, Math.floor(random(0, 5)));
+    grammarTimer = 0;
+  }
+  if(currentSentence){
+    currentSentence.drawSentence();
+  }
+  pop();
 }
